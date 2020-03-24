92f470597588b0062da1b72a64391c3821159cca
cabred01 <60081047+cabred01@users.noreply.github.com>
2020-03-23 20:19:53 -0400

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Edward A. Cabrera
+// IT 1050 - Programming Logic
+// Lab 1 - Hello World
+
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Edward A. Cabrera!");
+        }
+    }
+}
